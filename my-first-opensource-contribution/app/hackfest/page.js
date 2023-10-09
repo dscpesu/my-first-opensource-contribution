498020e71a8df75769e96e9894e08a5c534c4cc6
Larry8668 <114809719+Larry8668@users.noreply.github.com>
2023-10-09 16:19:09 +0530

    STUFF

---
+++ my-first-opensource-contribution/app/hackfest/page.js
@@ -0,0 +1,56 @@
+import Image from "next/image";
+import fs from "fs";
+import path from "path";
+
+function getAllContributions() {
+  const contributionsDir = path.resolve(process.cwd(), "contributions");
+
+  const fileNames = fs.readdirSync(contributionsDir);
+
+  const slugNames = [];
+
+  for (const fileName of fileNames) {
+    if (fileName.endsWith(".mdx")) {
+      const slug = path.basename(fileName, ".mdx");
+      slugNames.push(slug);
+    }
+  }
+
+  return slugNames;
+}
+
+export default function Home() {
+  const slugNames = getAllContributions();
+  console.log(slugNames);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-start ">
+      <div className="relative w-full h-[40vh] object-cover flex flex-col items-center justify-center gap-2    bg-[url('/hero-vector.png')]  ">
+        <Image
+          src={"/gdsc_logo.png"}
+          alt="gdsc-logo-title"
+          height={100}
+          width={100}
+        />
+        <h1 className=" text-2xl md:text-5xl text-gray-500 text-center">
+          Google Developer Student Clubs
+        </h1>
+        <p className="text-sm md:text-xl text-center  ">
+          Make Your First Open Source Contribution!
+        </p>
+      </div>
+      <div className="flex flex-col gap-3 items-center justify-center text-black my-6 px-9 text-center">
+        <h1 className="text-xl md:text-3xl text-gray-500 ">
+          Our Contributors along the way :
+        </h1>
+        <hr />
+        <div className="flex flex-wrap gap-5">
+          {slugNames.map((ele)=>{
+            return(<div className="my-2 p-2 border-2 border-gray-500 rounded text-gray-500 hover:text-black hover:border-black hover:scale-110 cursor-pointer transition-transform" >{ele}</div>)
+          })}
+        </div>
+        <hr />
+
+      </div>
+    </main>
+  );
+}
