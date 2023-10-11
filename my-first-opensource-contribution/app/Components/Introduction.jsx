26d3229ec0350d41551840b763a29ba83d104d71
Larry8668 <114809719+Larry8668@users.noreply.github.com>
2023-10-11 22:42:26 +0530

    added conditional rendering @Lehar
    added conditional 'a' or 'an' in contribution for role

--- my-first-opensource-contribution/app/Components/Introduction.jsx
+++ my-first-opensource-contribution/app/Components/Introduction.jsx
@@ -3,10 +3,11 @@ import React from 'react'
 const Introduction = ({ name,
     role,
     semester }) => {
+        const vowels = ['a', 'e', 'i', 'o', 'u'];
     return (
         <div className='flex flex-col items-center justify-center gap-y-2'>
             { name!=null && <p className="text-3xl md:text-5xl text-center">✨ Hello There, I am {name}! </p>}
-            { role!=null && <p className="text-2xl md:text-3xl text-center">I am a {role}🚀 </p>}
+            { role!=null && <p className="text-2xl md:text-3xl text-center">I am {(vowels.includes(role[0].toLowerCase()))?'an':'a'} {role}🚀 </p>}
             { semester!=null && <p className="text-xl md:text-2xl text-center">I am currently in my {semester} semester!</p>}
         </div>
     )
